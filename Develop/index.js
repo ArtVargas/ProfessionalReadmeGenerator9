c04e9683292f9645e05c79c05d852e6a7d3dd740
Arturo <arturodamianvargas1996@gmail.com>
2023-10-23 17:18:20 -0500

    Added const array questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,119 @@ const fs= required ("fs");
 const inquirer= require ("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+{
+    type:"input",
+    message: "What is your project titled?",
+    name: "title",
+},
+
+{
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose of this project. Give a detailed description of your project",
+    
+},
+
+{
+    type: "input",
+    name: "Table of Contents",
+    message: "Table of Contents",
+
+},
+
+{
+    type: "input",
+    name: "Installation",
+    message: "What does the user need to do in order to install the app to run properly?",
+
+},
+
+{
+
+    type: "input",
+    name: "Usage",
+    message: "How is the app used? Please give instructions",
+
+},
+
+{
+    type: "checkbox",
+    name: "license",
+    message: "Please select an applicable license for this project.",
+    options: ["MIT", "Apache2.0", "GNU General Public License v3.0", "BSD2", "BSD3", "Eclipse Public License","None"],
+},
+
+{
+    type: "input",
+    name: "Contribution",
+    message: "Who contributed to this project?",
+
+},
+
+{
+    type: "input",
+    name: "Tests",
+    message: "What commands are needed in order to run this application?",
+
+},
+
+{
+    type: "input",
+    name: "Questions",
+    message: "Contact info for inquiries",
+
+},
+
+{
+    type: "input",
+    name: "Github Username",
+    message: "What is your github username?",
+
+},
+
+    {
+        type: "input",
+        name: "Email Address",
+        message: "What is your email address?",
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
